f7f7a7535223d6153e77902b44304255efebb1db
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-11 13:26:51 -0300

    mudando o tema light e usando google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
@@ -12,6 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      //adicioando meu próprio tema
+      theme: ThemeData(
+          textTheme: GoogleFonts.bitterTextTheme(),
+          primarySwatch: Colors.grey,
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Colors.amber,
+              titleTextStyle: TextStyle(
+                color: Colors.white,
+              ))),
+
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
