65fc3283f169b0843bd2bb562f6ef3990ff78d69
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-12 09:41:50 -0300

    adicionando interceptador no projeto

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,19 +1,24 @@
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
 
 class JournalService {
   static const String url = "http://192.168.15.1:3000/";
   static const String resource = "learnhttp";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   Uri getUri() {
     return Uri.parse("$url$resource");
   }
 
   register(String content) {
-    http.post(getUri(), body: {"content": content});
+    client.post(getUri(), body: {"content": content});
   }
 
   Future<String> get() async {
-    http.Response response = await http.get(getUri());
+    http.Response response = await client.get(getUri());
     return response.body;
   }
 }
